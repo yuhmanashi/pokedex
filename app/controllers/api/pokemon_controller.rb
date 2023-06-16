8040be2191edaab6836c57ef5da1133745ebcaeb
yuhmanashi <yzxu28@gmail.com>
2023-06-16 00:12:33 -0700

    finished pokemon, may come back to work on moves in the future

--- app/controllers/api/pokemon_controller.rb
+++ app/controllers/api/pokemon_controller.rb
@@ -1,4 +1,7 @@
 class Api::PokemonController < ApplicationController
+    wrap_parameters include: Pokemon.attribute_names + ['imageUrl']
+    alias_attribute :type, :poke_type
+    
     def types
         render json: Pokemon::TYPES
     end
@@ -14,11 +17,29 @@ class Api::PokemonController < ApplicationController
     end
 
     def create
+        @pokemon = Pokemon.new(pokemon_params)
 
+        if @pokemon.save
+            render :show
+        else
+            render json: @pokemon.errors.messages, status: :unprocessable_entity
+        end
     end
 
 
     def update
+        @pokemon = Pokemon.find(params[:id])
         
+        if @pokemon.update(pokemon_params)
+            render :show
+        else
+            render json: @pokemon.errors.messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:id, :number, :name, :attack, :defense, :poke_type, :image_url, :captured)
     end
 end
